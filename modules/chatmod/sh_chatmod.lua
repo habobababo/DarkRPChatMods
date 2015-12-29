4375507489fa9739c98f813a004d87d0f28b3a3f
habobababo <shadowman11221@gmail.com>
2015-12-29 16:07:25 +0100

    Create sh_chatmod.lua

---
+++ modules/chatmod/sh_chatmod.lua
@@ -0,0 +1,25 @@
+// assign the commands
+
+DarkRP.declareChatCommand{
+	command = "anonymous",
+	description = "Write anonymous!",
+	delay = 1.5
+}
+
+DarkRP.declareChatCommand{
+	command = "anon",
+	description = "Write anonymous!",
+	delay = 1.5
+}
+
+DarkRP.declareChatCommand{
+	command = "hidden",
+	description = "Write anonymous!",
+	delay = 1.5
+}
+
+DarkRP.declareChatCommand{
+    command = "advert",
+    description = "Advertise something to everyone in the server.",
+    delay = 1.5
+}
